6aa93b8b786858f7bd5eed49e7242870ae3577c3
DESKTOP-RL3JT52\popov <popovaleks444@gmail.com>
2025-07-03 18:47:23 +0300

    Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool canInstantiate = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInstantiate)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                StartCoroutine(DisableInputForAWhile());
+            }
+        }
+    }
+
+    IEnumerator DisableInputForAWhile()
+    {
+        canInstantiate = false;
+        yield return new WaitForSeconds(1f);
+        canInstantiate = true;
+    }
+}
---
+++ Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+
+    void Start()
+    {
+        Invoke("SpawnRandomBall", startDelay);
+    }
+
+    void SpawnRandomBall()
+    {
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        var repeatRate = Random.Range(3, 5);
+        Invoke("SpawnRandomBall", repeatRate);
+        Debug.Log(repeatRate);
+    }
+}
