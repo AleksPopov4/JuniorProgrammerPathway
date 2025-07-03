822feb09e16a0cf834a38a91a26e45da01f0aa56
DESKTOP-RL3JT52\popov <popovaleks444@gmail.com>
2025-07-03 16:26:41 +0300

    Finish Prototype 2

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] animalPrefabs; 
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 16.0f;
+    private float spawnPosZ = 20.0f;
+    private float startDelay = 2.0f;
+    private float repeatRate = 1.5f;
 
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, repeatRate); // Start spawning animals after 2 seconds, repeat every 1.5 seconds
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+        }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
